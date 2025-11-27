411b429ee7507b9ee29d645c9b2edc2ddeda902b
Alfist552 <ndetsura@mail.ru>
2025-11-27 13:55:05 +0300

    Добавление функций для поиска и проверки MAC-адресов

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import re
-import urllib.request
 import os
-from typing import List
+from typing import List, Tuple
 
 class MacAddress:
     """Классы для работы с Mac-адресами"""
@@ -20,3 +19,8 @@ class MacAddress:
     def is_valid_mac(self, mac_address: str) -> bool:
         """Проверкка является ли строка MAC-адресом"""
         return bool(self.Strict_MAC_Pattern.match(mac_address.strip()))
+
+    def extract_mac_from_text(self, text: str) -> Tuple[List[str], List[str]]:
+        all_found = self.find_mac_address(text)
+        valid_macs = [mac for mac in all_found if self.is_valid_mac(mac)]
+        return valid_macs, all_found
