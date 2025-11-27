3c081fa14067b9c56ddc736500606cd50e27c883
Alfist552 <ndetsura@mail.ru>
2025-11-27 13:34:28 +0300

    Добавление проверки и валидации MAC-адресов(На первых этапах проверка только формата ХХ-ХХ-ХХ-ХХ-ХХ и ХХ:ХХ:ХХ:ХХ:ХХ

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import re
 import urllib.request
 import os
+
+class MacAddress:
+    """Классы для работы с Mac-адресами"""
+
+    MAC_Pattern = re.compile( #Поиск MAC-адресов
+        r'(?:[0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})]' #Формат ХХ:ХХ:ХХ:ХХ:ХХ и ХХ-ХХ-ХХ-ХХ-ХХ
+    )
+
+    Strict_MAC_Pattern = re.compile( #Валидация MAC-адресов
+        r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$|'
+    )
